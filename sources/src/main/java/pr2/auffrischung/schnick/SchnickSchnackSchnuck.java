c9622699f44f3c077dc6a7ad2e0caa9b54a2319c
“Majd <majdebrahem7@gmail.com>
2025-10-07 00:44:56 +0200

    Die Methoden wurden geschrieben, dazu habe ich noch zwei neue Methoden hinzugefügt. Es fehlt aber die Korrektur bei der evaluateRound-Methode, viel Spaß dabei :)

--- sources/src/main/java/pr2/auffrischung/schnick/SchnickSchnackSchnuck.java
+++ sources/src/main/java/pr2/auffrischung/schnick/SchnickSchnackSchnuck.java
@@ -18,10 +18,14 @@ public class SchnickSchnackSchnuck {
         // Drei Runden spielen
         for (int round = 1; round <= 3; round++) {
             // Spieler eingeben lassen
-            char playerMove = getPlayerMove(scanner);
+            char playerMove = getPlayerMove(round, scanner);
             
             // Computerzug generieren
             int computerMove = getComputerMove(random);
+            
+            String playerMoveString = playerMovesToStrings(playerMove);
+            
+            String computerMoveString = computerMovementsToStrings(computerMove);
             
             // Ergebnis der Runde auswerten
             int roundResult = evaluateRound(playerMove, computerMove);
@@ -34,7 +38,7 @@ public class SchnickSchnackSchnuck {
             }
             
             // Runde auswerten und anzeigen
-            displayRoundResult(round, playerMove, computerMove, roundResult, playerPoints, computerPoints);
+            displayRoundResult(round, playerMove, computerMove, roundResult, playerPoints, computerPoints, playerMoveString, computerMoveString);
         }
         
         // Endpunktestand anzeigen
@@ -42,21 +46,47 @@ public class SchnickSchnackSchnuck {
     }
 
     // Methode um den Zug des Spielers zu erhalten
-    public static char getPlayerMove(Scanner scanner) {
-        // TODO: Implementiere Eingabeaufforderung und Rückgabe des Spielerzuges
-        
-        //Majd
-        
-        return ' '; // Platzhalter, muss ersetzt werden
+    public static char getPlayerMove(int round, Scanner scanner) {
+        System.out.println();
+        System.out.print("Runde " + round + ": Dein Zug ([r]ock, [p]aper, [s]cissors)? ");
+        char playerMove = scanner.next().charAt(0);
+
+        while (playerMove != 'r' & playerMove != 'p' & playerMove != 's') {
+            System.err.println("Ungültige Eingabe, versuch es erneut!");
+            System.out.print("Runde " + round + ": Dein Zug ([r]ock, [p]aper, [s]cissors)? ");
+            playerMove = scanner.next().charAt(0);
+        } 
+            
+            return playerMove;
     }
 
     // Methode um den Zug des Computers zu bekommen
     public static int getComputerMove(Random random) {
-        // TODO: Zufällige Auswahl für den Computer (Rock, Paper, Scissors)
         
-        //Majd
+        int computerMove = random.nextInt(3)+1;
+        return computerMove;
+    }
+    
+    public static String playerMovesToStrings(char playerMove) {
         
-        return 0; // Platzhalter, muss ersetzt werden
+        switch(playerMove) {
+        case 'r' : return "ROCK";
+        case 'p' : return "PAPER";
+        case 's' : return "SCISSORS";
+        default  : return "Ungültig";
+        
+        }  
+    }
+    
+    public static String computerMovementsToStrings(int computerMove) {
+        
+        switch(computerMove) {
+        case ROCK : return "ROCK";
+        case PAPER : return "PAPER";
+        case SCISSOR : return "SCISSORS";
+        default: return "Ungültig";
+        
+        }
     }
 
     // Methode um das Ergebnis der Runde zu evaluieren
@@ -83,18 +113,32 @@ public class SchnickSchnackSchnuck {
         else
             return UNGUELTIG;
     }
+    
+    
 
     // Methode um das Ergebnis der Runde anzuzeigen
-    public static void displayRoundResult(int round, char playerMove, int computerMove, int roundResult, int playerPoints, int computerPoints) {
+    public static void displayRoundResult(int round, char playerMove, int computerMove, int roundResult, int playerPoints, int computerPoints
+            , String playerMoveString, String computerMoveString) {
         
-        //majd
         
-        // TODO: Ausgabe des Ergebnisses der Runde
-    }
+        if (roundResult == 1) {
+            System.out.print("Du hast gewonnen: ");
+        }
+        else if (roundResult == -1) {
+            System.out.print("Der Computer hat gewonnen - ");
+        }
+        else if (roundResult == 0) {
+            System.out.print("Unentschieden - ");
+        }
+        
+        System.out.println("Du: " + playerMoveString + ", Computer: " + computerMoveString + " - [" + playerPoints + ":" + computerPoints + "]");
+        
+        
+        }
 
     // Methode um den Endpunktestand anzuzeigen
     public static void displayFinalScore(int playerPoints, int computerPoints) {
-        System.out.println("Ergebnis - Du " + playerPoints + "Punkt(e), Computer " + computerPoints + "Punkt(e)");
+        System.out.println("Ergebnis - Du " + playerPoints + " Punkt(e), Computer " + computerPoints + "Punkt(e)");
         if(computerPoints < playerPoints)
             System.out.println("Du hast im Spiel gewonnen");
         else if(computerPoints > playerPoints)
